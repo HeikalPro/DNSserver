13c5ed60e5261b95ac40f998337e756222684e95
Ahmed Heikal <149324079+HeikalPro@users.noreply.github.com>
2024-07-07 22:45:27 +0300

    DNSServer.py

--- DNSServer.py
+++ DNSServer.py
@@ -32,14 +32,14 @@ def generate_aes_key(password, salt):
 def encrypt_with_aes(input_string, password, salt):
     key = generate_aes_key(password, salt)
     f = Fernet(key)
-    encrypted_data = f.encrypt(input_string.encode('utf-8'))
+    encrypted_data = f.encrypt(input_string)
     return encrypted_data
 
 def decrypt_with_aes(encrypted_data, password, salt):
     key = generate_aes_key(password, salt)
     f = Fernet(key)
     decrypted_data = f.decrypt(encrypted_data)
-    return decrypted_data.decode('utf-8')
+    return decrypted_data
 
 salt = b'Tandon'
 password = 'rka6642@nyu.edu'
